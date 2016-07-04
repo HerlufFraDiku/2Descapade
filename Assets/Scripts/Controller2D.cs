99fe91523d31d94c8081ccf707b3504930a380a7
Herluf <herluf@Herlufs-MacBook-Pro.local>
2016-07-04 22:15:12 +0200

    initial commit

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+	BoxCollider2D boxCollider;
+	RaycastOrigins rayOrigins;
+	public CollisionInfo collisions;
+
+	const float skinWidth = 0.015f;
+
+	public LayerMask collisionMask;
+	[Range(2, 256)] public int horizontalRayCount = 4;
+	[Range(2, 256)] public int verticalRayCount = 4;
+	float horizontalRaySpacing;
+	float verticalRaySpacing;
+	Vector2 playerInput;
+
+	void Start () {
+		boxCollider = GetComponent<BoxCollider2D> ();
+		CalculateRaySpacing ();
+	}
+
+
+	public void Move(Vector3 velocity, Vector2 input){
+		UpdateRayOrigins ();
+		collisions.Reset ();
+		playerInput = input;
+
+		if (!collisions.onRope) {
+			if (velocity.x != 0) {
+				HorizontalCollisions (ref velocity);
+			}
+
+			if (velocity.y != 0) {
+				VerticalCollisions (ref velocity);
+			}
+		}
+
+		if (collisions.onRope) {
+			transform.Translate(new Vector3(0, velocity.y, 0));
+		} else {
+			transform.Translate (velocity);
+		}
+	}
+
+	void HorizontalCollisions(ref Vector3 velocity){
+		float directionX = Mathf.Sign (velocity.x);
+		float rayLength = Mathf.Abs (velocity.x) + skinWidth;
+
+		for (int i = 0; i < horizontalRayCount; i++) {
+			Vector3 rayOrigin = (directionX == -1)?rayOrigins.bottomLeft:rayOrigins.bottomRight;
+			rayOrigin += Vector3.up * (horizontalRaySpacing * i);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector3.right * directionX, rayLength, collisionMask);
+
+			Debug.DrawRay (rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+			if (hit) {
+				if (hit.collider.tag == "Jumpable") {
+					if (hit.distance == 0) {
+						continue;
+					}
+				}
+
+				if (hit.collider.tag == "Climpable") {
+					if (playerInput.y == 1 && !collisions.onRope) {
+						collisions.onRope = true;
+						transform.position = new Vector3 (hit.transform.position.x, transform.position.y, 0);
+					}
+					continue;
+				}
+
+				velocity.x = (hit.distance - skinWidth) * directionX;
+				rayLength = hit.distance;
+
+				collisions.left = directionX == -1f;
+				collisions.right = directionX == 1f;
+			}
+		}
+	}
+
+	void VerticalCollisions(ref Vector3 velocity){
+		float directionY = Mathf.Sign (velocity.y);
+		float rayLength = Mathf.Abs (velocity.y) + skinWidth;
+
+		for (int i = 0; i < verticalRayCount; i++) {
+			Vector3 rayOrigin = (directionY == -1)?rayOrigins.bottomLeft:rayOrigins.topLeft;
+			rayOrigin += Vector3.right * (verticalRaySpacing * i + velocity.x);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector3.up * directionY, rayLength, collisionMask);
+
+			Debug.DrawRay (rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+			if (hit) {
+				if (hit.collider.tag == "Jumpable") {
+					if (directionY == 1 || hit.distance == 0 || (playerInput.y == -1 && Input.GetKeyDown(KeyCode.Space))) {
+						continue;
+					}
+				}
+
+				if (hit.collider.tag == "Climpable") {
+					if (playerInput.y == 1 && !collisions.onRope) {
+						collisions.onRope = true;
+						transform.position = new Vector3 (hit.transform.position.x, transform.position.y, 0);
+					}
+					continue;
+				}
+
+				velocity.y = (hit.distance - skinWidth) * directionY;
+				rayLength = hit.distance;
+			
+				collisions.below = directionY == -1f;
+				collisions.above = directionY == 1f;
+
+			}
+		}
+	}
+
+	void UpdateRayOrigins(){
+		Bounds bounds = boxCollider.bounds;
+		bounds.Expand (skinWidth * -2f);
+
+		rayOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		rayOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+		rayOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		rayOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+	}
+
+	void CalculateRaySpacing(){
+		Bounds bounds = boxCollider.bounds;
+		bounds.Expand (skinWidth * -2f);
+
+		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+	}
+
+
+	public struct RaycastOrigins {
+		public Vector2 topLeft, topRight;
+		public Vector2 bottomLeft, bottomRight;
+	}
+
+	public struct CollisionInfo{
+		public bool above, below;
+		public bool left, right;
+		public bool onRope;
+
+		public void Reset(){
+			above = below = false;
+			left = right = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour {
+
+	Controller2D controller;
+
+	public float maxJumpHeight = 4f;
+	public float minJumpHeight = 2f;
+	public float timeToJumpApex = .3f;
+	public float moveSpeed = 6f;
+	public float accelerationGrounded = .1f;
+	public float accelerationAirborne = .2f;
+
+	float gravity;
+	float maxJumpVelocity;
+	float minJumpVelocity;
+	Vector3 velocity;
+	float xSmoothing;
+
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+		gravity = -(2f * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		maxJumpVelocity = Mathf.Abs (gravity) * timeToJumpApex; 
+		minJumpVelocity = Mathf.Sqrt (2f * Mathf.Abs (gravity) * minJumpHeight);
+	}
+	
+
+	void Update () {
+
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (controller.collisions.below || controller.collisions.above || controller.collisions.onRope) {
+			velocity.y = 0;
+		}
+
+		if (input.y != -1 && Input.GetKeyDown (KeyCode.Space) && controller.collisions.below) {
+			velocity.y = maxJumpVelocity;
+		}
+
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			if (velocity.y > minJumpVelocity) {
+				velocity.y = minJumpVelocity;
+			}	
+		}
+			
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref xSmoothing, (controller.collisions.below) ? accelerationGrounded : accelerationAirborne);
+
+		if (!controller.collisions.onRope) {
+			velocity.y += gravity * Time.deltaTime;
+		}
+
+		controller.Move (velocity * Time.deltaTime, input);
+	}
+}
